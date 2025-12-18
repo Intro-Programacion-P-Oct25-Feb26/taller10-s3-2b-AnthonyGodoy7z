8f40d07050d24348acc53360cbe98b26fc8a45f8
Anthony <tonyxiz10@gmail.com>
2025-12-18 12:44:57 -0500

    resolucion parcial del taller

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,25 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[][] a = new int[3][2];
+        int[][] b = new int[3][2];
+
+        System.out.println("Ingrese valores de la matriz A");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("ingrese valor de (%d)(%d): ", i, j);
+                a[i][j] = entrada.nextInt();
+            }
+        }
+    }
 }
