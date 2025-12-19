0ae57b06dddc323a841a9b6e3a8630c76e1f05b5
Anthony <tonyxiz10@gmail.com>
2025-12-18 22:30:14 -0500

    commit

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -5,10 +5,44 @@
  */
 package paquete6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema06 {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double[][] matriz = new double[5][6];
+        double[][] resultado = new double[5][6];
+        double num;
+
+        System.out.println("Ingrese un número entero o real");
+        num = entrada.nextDouble();
+
+        System.out.println("Ingrese valores a la matriz de 5 filas y 6 columnas");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Ingrese valor de [%d][%d]:", i, j);
+                matriz[i][j] = entrada.nextDouble();
+                resultado[i][j] = matriz[i][j] * num;
+            }
+        }
+        System.out.println("Matriz original");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%.2f\t", matriz[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("Matriz multiplicada por (" + num+")");
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.printf("%.2f\t", resultado[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
